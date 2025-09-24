93143fbc61f0142717fd5babf69da016dbab68ba
TheMoctha <riveraromanjoseandres@gmail.com>
2025-09-24 12:30:25 -0500

    Se sube la base del proyecto

---
+++ src/main/java/com/Encriptador/Encriptador/EncriptadorApplication.java
@@ -0,0 +1,13 @@
+package com.Encriptador.Encriptador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncriptadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncriptadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Encriptador/Encriptador/EncriptadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Encriptador.Encriptador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncriptadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
